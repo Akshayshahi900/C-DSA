8fafa9559dd7e992a5f31a70cb5a9f31f5cb1c5e
Akshayshahi900 <shahiakshay900@gmail.com>
2024-10-26 19:45:12 +0530

    Updated

--- recursionp1.cpp
+++ recursionp1.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
 void print(int n)
-{
+{ // base case
     if (n == 0)
         return;
 
-    cout << n << endl;
+    // recursive call
     print(n - 1);
+
+    cout << n << endl;
+    // recursive relation
 }
 int power(int n)
 {
