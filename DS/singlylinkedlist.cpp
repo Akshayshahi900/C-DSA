72c6ebb5ab14bce6303db5edf45b3e8968921e1d
Akshayshahi900 <shahiakshay900@gmail.com>
2024-12-17 08:47:24 +0530

    updated

--- DS/singlylinkedlist.cpp
+++ DS/singlylinkedlist.cpp
@@ -37,21 +37,63 @@ void print(Node *&head)
     }
     cout << endl;
 }
+void insertAtTail(Node *&tail, int d)
+{
+    Node *temp = new Node(d);
+    tail->next = temp;
+    tail = tail->next;
+}
+void insertAtPostion(Node *&tail, Node *&head, int position, int d)
+{
+    // if insertion is at head;
+    if (position == 1)
+    {
+        insertAtHead(head, d);
+        return;
+    }
+
+    Node *temp = head;
+    int cnt = 1;
+
+    while (cnt < position - 1)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+
+    // insertion at end of the list
+    if (temp->next == NULL)
+    {
+        insertAtTail(tail, d);
+        return;
+    }
+
+    // create a new node
+    Node *nodeToInsert = new Node(d);
+    nodeToInsert->next = temp->next;
+    temp->next = nodeToInsert;
+}
 int main()
 { // create a new node
     Node *node1 = new Node(10);
     Node *head = node1;
+    Node *tail = node1;
     // cout << Node1->data << endl;
     // cout << Node1->next << endl;
     // head pointed to node1
     print(head);
-    insertAtHead(head, 12);
+    // insertAtHead(head, 12);
+    insertAtTail(tail, 12);
 
     print(head);
-    insertAtHead(head, 13);
+    insertAtTail(tail, 13);
 
     print(head);
-    insertAtHead(head, 14);
+    insertAtTail(tail, 14);
 
     print(head);
+    insertAtPostion(tail, head, 5, 22);
+    print(head);
+    cout << "Head: " << head->data << endl;
+    cout << "Tail: " << tail->data << endl;
 }
